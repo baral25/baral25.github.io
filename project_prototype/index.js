b926e0531c7cbc5e14f763cb76244c821eeccfa2
Aaroha Baral <49807333+baral25@users.noreply.github.com>
2023-10-14 12:49:11 +0800

    Add files via upload
    Cleaned nav bar and footers and re wrote non-responsive divs

--- project_prototype/index.js
+++ project_prototype/index.js
@@ -6,36 +6,93 @@ document.addEventListener("DOMContentLoaded", function () {
         maxZoom: 19,
         attribution: '© OpenStreetMap'
     }).addTo(map);
-
+    var goldIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+      var redIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+      var blueIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
     const markers = {
-        "row1": L.marker([-32.00366103481073, 115.89282585477542]),
-        "row2": L.marker([-32.00959407967216, 115.89185757204376]),
-        "row3": L.marker([-32.00979407967216, 115.89385757204376])
+        "row1": L.marker([-32.00366103481073, 115.89282585477542],{icon:goldIcon}),
+        "row2": L.marker([-32.00959407967216, 115.89185757204376],{icon:redIcon}),
+        "row3": L.marker([-32.00979407967216, 115.89385757204376],{icon:blueIcon})
     };
 
     const markerGroup = L.layerGroup(Object.values(markers)).addTo(map);
+
+    // Function to handle row click and navigate to a new page
+    function handleRowClick(rowKey) {
+        navigateToPage(rowKey);
+
+        // Remove the 'highlight' class from all table rows
+        const tableRows = document.querySelectorAll('tbody tr');
+        tableRows.forEach(row => {
+            row.classList.remove('table-success');
+        });
+
+        // Find the corresponding table row by matching the marker key
+        const tableRow = document.querySelector(`tbody tr[data-key="${rowKey}"]`);
+
+        // Add the 'highlight' class to the corresponding table row
+        if (tableRow) {
+            tableRow.classList.add('table-success');
+            // Scroll to the highlighted table row
+            tableRow.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
 
     // Add a click event listener to each marker
     Object.keys(markers).forEach(key => {
         const marker = markers[key];
         marker.on('click', function () {
-            // Remove the 'highlight' class from all table rows
-            const tableRows = document.querySelectorAll('tbody tr');
-            tableRows.forEach(row => {
-                row.classList.remove('table-success');
-            });
-
-            // Find the corresponding table row by matching the marker key
-            const tableRow = document.querySelector(`tbody tr[data-key="${key}"]`);
-
-            // Add the 'highlight' class to the corresponding table row
-            if (tableRow) {
-                tableRow.classList.add('table-success');
-                // Scroll to the highlighted table row
-                tableRow.scrollIntoView({ behavior: 'smooth' });
-            }
+            handleRowClick(key);
         });
     });
+
+    // Add click event listeners to table rows
+    const tableRows = document.querySelectorAll('tbody tr');
+
+    tableRows.forEach(row => {
+        row.addEventListener('click', function () {
+            const rowKey = row.getAttribute('data-key');
+            handleRowClick(rowKey);
+        });
+    });
+
+    // Function to handle row click and navigate to a new page
+    function navigateToPage(rowKey) {
+        // Define the mapping of row keys to page URLs
+        const pageURLs = {
+            row1: 'page1.html',
+            row2: 'page2.html',
+            row3: 'page3.html'
+        };
+
+        // Get the URL associated with the clicked row
+        const targetURL = pageURLs[rowKey];
+
+        if (targetURL) {
+            window.location.href = targetURL; // Navigate to the URL
+        }
+    }
 
     // Check the current time and change text color accordingly
     function checkOpenStatus() {
